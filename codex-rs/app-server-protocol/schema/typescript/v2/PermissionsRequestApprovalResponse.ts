openai/codex

Add request permissions tool

codex-rs/app-server-protocol/schema/typescript/v2/PermissionsRequestApprovalResponse.ts
@@ -1,0 +1,6 @@
+// GENERATED CODE! DO NOT MODIFY BY HAND!
+
+// This file was generated by [ts-rs](https://github.com/Aleph-Alpha/ts-rs). Do not edit this file manually.
+import type { AdditionalPermissionProfile } from "./AdditionalPermissionProfile";
+
+export type PermissionsRequestApprovalResponse = { permissions: AdditionalPermissionProfile, };

njfio/codex-process

codex-rs/app-server-protocol/schema/typescript/v2/PermissionsRequestApprovalResponse.ts
@@ -1,0 +1,6 @@
+// GENERATED CODE! DO NOT MODIFY BY HAND!
+
+// This file was generated by [ts-rs](https://github.com/Aleph-Alpha/ts-rs). Do not edit this file manually.
+import type { GrantedPermissionProfile } from "./GrantedPermissionProfile";
+
+export type PermissionsRequestApprovalResponse = { permissions: GrantedPermissionProfile, };